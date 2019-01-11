b84a404aa77183c4c250e419cb8d61d7be7ee53a
robin <robin.guov@gmail.com>
2019-01-11 14:47:19 +0800

    fix

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  host: process.env.HOST || '139.180.146.32',
+  host: process.env.HOST || '127.0.0.1',
   port:
     process.env.PORT || (process.env.NODE_ENV === 'production' ? 8080 : 3000),
   apiHost: process.env.APIHOST || '127.0.0.1',
